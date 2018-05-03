8ec5754bbf2a0eed04d8f7fc1e8ff1a9a608107c
vladsaveliev <vladislav.sav@gmail.com>
2018-05-03 22:14:22 +1000

    No-docker install: fix install_github for linux; set perms for R scripts

--- install_no_docker/install.sh
+++ install_no_docker/install.sh
@@ -1,3 +1,9 @@
+# Install all PCGR dependencies to avoid using the Docker image.
+# Suitable for HPC systems lacking Docker, and for debugging.
+# Works only for Linux due to unavaliability of few packages for MacOS (ensembl-vep -> perl-bio-db-hts)
+
+SRC_DIR=../src
+
 # Install conda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p ./miniconda
@@ -7,14 +13,19 @@ export PATH=$(pwd)/miniconda/bin:$PATH
 conda env create --file conda_environment.yml
 source activate pcgr
 
-# Few more R packages that are not on yet conda (TODO: add them):
-R -e "library(devtools); devtools::install_github('mjkallen/rlogging'); devtools::install_github('kent37/summarywidget')"
+# Few more R packages that are not on yet conda (TODO: add them).
+# The "options(unzip = )" hack to address the install_github issue under conda https://github.com/r-lib/devtools/issues/1722
+R -e "library(devtools); options(unzip = '$(which unzip)'); devtools::install_github('mjkallen/rlogging')"
+R -e "library(devtools); options(unzip = '$(which unzip)'); devtools::install_github('kent37/summarywidget')"
+# This one is local
+R -e "library(devtools); devtools::install('${SRC_DIR}/R/pcgrr')"
 
 # Install VEP separately (doesn't work when within the envirnoment file, for some reason):
 conda install -c bioconda -y ensembl-vep
 vep_install -a ap -g miRNA -l -n
 
 # Access to src scripts
-chmod +x ../src/pcgr/*.py
-export PATH=$PATH:$(pwd)/../src/pcgr
-export PYTHONPATH=$(pwd)/../src/pcgr/lib:${PYTHONPATH}
+chmod +x ${SRC_DIR}/pcgr/*.py
+chmod +x ${SRC_DIR}/*.R
+export PATH=$PATH:$(pwd)/${SRC_DIR}/pcgr
+export PYTHONPATH=$(pwd)/${SRC_DIR}/lib:${PYTHONPATH}
