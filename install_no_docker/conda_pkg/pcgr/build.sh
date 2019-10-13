63d98d06f4513572ddf5196f226af34b2de54868
vladsaveliev <vladislav.sav@gmail.com>
2019-10-13 15:01:17 +1100

    Conda pkg: manual R packages installation fixes

--- install_no_docker/conda_pkg/pcgr/build.sh
+++ install_no_docker/conda_pkg/pcgr/build.sh
@@ -21,10 +21,10 @@ mv ${SRC_DIR}/src/pcgr/*.py ${SRC_DIR}/*.py ${PREFIX}/bin/  # python scripts
 mv ${SRC_DIR}/src/*.R ${PREFIX}/bin/  # R scripts
 # R modules:
 R -e "library(devtools); devtools::install('${SRC_DIR}/src/R/pcgrr', dependencies=FALSE, args=c('--library=${PREFIX}/lib/R/library'))"
-R -e "library(devtools); devtools::install_github('rstudio/crosstalk', dependencies=FALSE, args=c('--library=${PREFIX}/lib/R/library'))"
-R -e "library(devtools); install.packages('BiocManager', repos = 'http://cran.us.r-project.org'), dependencies=FALSE, args=c('--library=${PREFIX}/lib/R/library'))"
-R -e "BiocManager::install('TxDb.Hsapiens.UCSC.hg19.knownGene')"
-R -e "BiocManager::install('TxDb.Hsapiens.UCSC.hg38.knownGene')"
+R -e "library(devtools); options(unzip = '$(which unzip)'); devtools::install_github('rstudio/crosstalk', dependencies=FALSE, args=c('--library=${PREFIX}/lib/R/library'))"
+R -e "install.packages('BiocManager', repos = 'http://cran.us.r-project.org', dependencies=FALSE, args=c('--library=${PREFIX}/lib/R/library'))"
+R -e "library(BiocManager); BiocManager::install('TxDb.Hsapiens.UCSC.hg19.knownGene')"
+R -e "library(BiocManager); BiocManager::install('TxDb.Hsapiens.UCSC.hg38.knownGene')"
 
 # VCF validator
 wget https://github.com/EBIvariation/vcf-validator/releases/download/v0.6/vcf_validator -O ${PREFIX}/bin/vcf_validator
