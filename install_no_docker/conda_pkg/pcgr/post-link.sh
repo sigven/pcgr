c782226a411f25e639405032241521e0affa3e00
pdiakumis <peterdiakumis@gmail.com>
2021-12-03 21:21:04 +1100

    remove install_no_docker dir

--- install_no_docker/conda_pkg/pcgr/post-link.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-#R -e "remove.packages('crosstalk')"
-#export TAR=$(which tar)
-#R -e "library(devtools); options(unzip = 'internal'); devtools::install_github('rstudio/crosstalk', ref = '68b0b617ee82e5d6b738f26106933254ce5ede53', dependencies=FALSE)"
