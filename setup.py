0f5769ffe90cf1b785a4168e64c448f064c6831c
Sigve Nakken <sigven@ifi.uio.no>
2022-01-17 17:25:13 +0100

    Bump version: 0.10.5 → 0.10.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pcgr',
-    version='0.10.5', # versioned by bump2version
+    version='0.10.6', # versioned by bump2version
     license='MIT',
     author='Sigve Nakken',
     author_email='sigven@gmail.com',
