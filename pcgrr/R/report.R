888e7fa578e3a8a7982878dc582ae7d567928f51
Sigve Nakken <sigven@ifi.uio.no>
2022-01-19 13:45:59 +0100

    - proper listing of bundle version

--- pcgrr/R/report.R
+++ pcgrr/R/report.R
@@ -230,9 +230,12 @@ set_report_metadata <- function(config,
 
   report_metadata <- list()
   report_metadata[["pcgr_db_release"]] <- pcgr_data[["release_notes"]]
-  report_metadata[['pcgr_db_release']][["bundle_version"]] <- list()
-  report_metadata[["pcgr_db_release"]][["bundle_version"]][["version"]] <-
-    config[['required_args']][['pcgr_bundle_version']]
+  report_metadata[["pcgr_db_release"]][["bundle_version"]] <-
+    list('url' = 'https://github.com/sigven',
+         'description' = 'PCGR databundle',
+         'version' = config[['required_args']][['pcgr_bundle_version']],
+         'name' = 'pcgr_bundle_version',
+         'resource_type' = 'db')
   report_metadata[["pcgr_version"]] <- config[['required_args']][['pcgr_version']]
   report_metadata[["cpsr_version"]] <- config[['required_args']][['cpsr_version']]
   report_metadata[["genome_assembly"]] <-
