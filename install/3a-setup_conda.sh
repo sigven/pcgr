955d79022d2c25b6256788a50efc20937a4bd81f
pdiakumis <peterdiakumis@gmail.com>
2022-02-02 20:52:11 +1100

    more installation info

--- install/3a-setup_conda.sh
+++ install/3a-setup_conda.sh
@@ -10,7 +10,8 @@ fi
 MINICONDA_URL="https://repo.continuum.io/miniconda/Miniconda3-latest-${PLATFORM}-x86_64.sh"
 
 wget ${MINICONDA_URL} -O miniconda.sh && chmod +x miniconda.sh
-# Follow prompts
+
+# Follow the prompts, then open new terminal session
 bash miniconda.sh
 
 # If you want faster conda package installations, use mamba after installing conda:
--- install/3b-create_conda_env.sh
+++ install/3b-create_conda_env.sh
@@ -7,10 +7,10 @@ else
     PLATFORM="linux-64"
 fi
 
-PCGR_CONDA_DIR="../conda"
+PCGR_CONDA_ENV_DIR="../conda/env"
 
-mamba create --prefix ${PCGR_CONDA_DIR}/env/pcgr --file ${PCGR_CONDA_DIR}/env/lock/pcgr-${PLATFORM}.lock
-mamba create --prefix ${PCGR_CONDA_DIR}/env/pcgrr --file ${PCGR_CONDA_DIR}/env/lock/pcgrr-${PLATFORM}.lock
+mamba create --prefix ${PCGR_CONDA_ENV_DIR}/pcgr --file ${PCGR_CONDA_ENV_DIR}/lock/pcgr-${PLATFORM}.lock
+mamba create --prefix ${PCGR_CONDA_ENV_DIR}/pcgrr --file ${PCGR_CONDA_ENV_DIR}/lock/pcgrr-${PLATFORM}.lock
 
 # activate pcgr conda environment
-conda activate ${PCGR_CONDA_DIR}/env/pcgr
+conda activate ${PCGR_CONDA_ENV_DIR}/pcgr
