2cb870ea1c8c46a9afd13d4e9c7f42db5fbebb16
pdiakumis <peterdiakumis@gmail.com>
2022-02-04 00:13:14 +1100

    remove redundant shell scripts

--- conda/condalock_env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-conda-lock --mamba -f env/yml/pcgr.yml -p osx-64 -p linux-64 --filename-template 'pcgr-{platform}.lock'
-conda-lock --mamba -f env/yml/pcgrr.yml -p osx-64 -p linux-64 --filename-template 'pcgrr-{platform}.lock'
