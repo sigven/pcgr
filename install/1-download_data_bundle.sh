69b818f1ea69246f0f87a7e5e3a110f2fab3679d
pdiakumis <peterdiakumis@gmail.com>
2022-02-10 22:10:02 +1100

    remove installation helper scripts

--- install/1-download_data_bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-#GENOME="grch37"
-GENOME="grch38"
-BUNDLE="pcgr.databundle.${GENOME}.20220119.tgz"
-
-wget http://insilico.hpc.uio.no/pcgr/${BUNDLE}
-gzip -dc ${BUNDLE} | tar xvf -
