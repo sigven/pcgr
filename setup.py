a9be3fb1d8ed64734f31fca392375e7949a9bc5f
pdiakumis <peterdiakumis@gmail.com>
2023-11-30 18:49:13 +1100

    Bump version: 1.4.5 → 1.4.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pcgr',
-    version='1.4.5', # versioned by bump2version
+    version='1.4.6', # versioned by bump2version
     license='MIT',
     author='Sigve Nakken',
     author_email='sigven@gmail.com',
