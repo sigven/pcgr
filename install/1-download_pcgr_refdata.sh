8caf5fd2a3215cd16c6f87a800857c61f06bc837
Sigve Nakken <sigven@ifi.uio.no>
2024-08-01 16:20:52 +0200

    updated download script

--- install/1-download_pcgr_refdata.sh
+++ install/1-download_pcgr_refdata.sh
@@ -1,5 +1,5 @@
 GENOME="grch38" # or "grch37"
-BUNDLE_VERSION="20240610"
+BUNDLE_VERSION="20240621"
 BUNDLE="pcgr_ref_data.${BUNDLE_VERSION}.${GENOME}.tgz"
 
 wget https://insilico.hpc.uio.no/pcgr/${BUNDLE}
