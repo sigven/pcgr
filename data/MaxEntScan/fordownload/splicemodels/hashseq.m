4289c9099bab8526df67e1902eff7f76d2744aa4
pdiakumis <peterdiakumis@gmail.com>
2025-03-04 09:51:56 +1100

    remove MaxEntScan data - already in loftee conda share

--- data/MaxEntScan/fordownload/splicemodels/hashseq.m
+++
@@ -1,8 +0,0 @@
-function [sum ] = hashseq(seq) 
-    sum = 0;
-    len = length(seq);
-    four = [1;4;16;64;256;1024;4096];
-    for i = 1:len
-        sum = sum + str2num(seq(i)) * four(len - i +1) ;
-    end
-    
